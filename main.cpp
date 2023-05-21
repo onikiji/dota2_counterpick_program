1f35c414327c66e414a6c0b3a6dc4934ca5db316
鬼 <intelgeforcemilky88@gmail.com>
2023-05-21 17:34:15 +0300

    First commit

--- main.cpp
+++ main.cpp
@@ -2,16 +2,29 @@
 using namespace std;
 
 int main() {
-	cout << "dota 2 counterpick" << endl <<"21.05.2023" << endl << endl << endl << "1.pudge" << endl << "2.tinker" << endl << "3.rubick" << endl << endl<< "choose hero number:" << endl << endl;
-	int a;
-	cin >> a;
-	if (a == 1)
-		cout << endl<<"pudge counterpick are: rubick/antient apparation/slark";
-	else if (a == 2)
-		cout << endl << "counterpick are: clockwerk/legion commander/storm spirit";
-	else if (a == 3)
-		cout << endl << "counterpick are: silencer/antimage/templar assasin";
-	else
-		cout << endl << "wrong hero number, stopping program...";
+	cout << "dota 2 counterpick" << endl <<"21.05.2023"<<endl<< "----------------------------------"<<endl<< "1.pudge" << endl << "2.tinker" << endl << "3.rubick" << endl << "----------------------------------";
+	int b,a;
+	int c = 2;	
+	for (b = 1; b <c;) {
+		cout << endl << "choose hero number:" << endl;
+		cin >> a;
+		if (a == 1) {
+			cout<< "pudge counterpick are: rubick/antient apparation/slark" << endl;
+			c = 1;
+		}
+		else if (a == 2) {			
+			cout << "counterpick are: clockwerk/legion commander/storm spirit" << endl;
+			c = 1;
+		}
+		else if (a == 3) {			
+			cout<< "counterpick are: silencer/antimage/templar assasin"<<endl;
+			c = 1;
+		}
+		else {			
+				cout<< "wrong hero number, try again"<<endl<<"----------------------------------";
+				c = 2;
+		}
+	}
 	return 0;
 }
+  
